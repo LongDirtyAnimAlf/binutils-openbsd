eeec398f326ea2f9eaa26e0089a7c5db61f1ff0d
Don <LongDirtyAnimAlf@gmail.com>
2023-02-25 07:53:52 +0000

    Re-organize repo.

---
+++ binutils-2.17/pacman_packages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "*** Getting needed packages ***"
+echo ""
+
+pacman -S mc git texinfo bison flex --noconfirm
+
+pacman -S mingw-w64-i686-binutils --noconfirm
+pacman -S mingw-w64-i686-gcc --noconfirm
+pacman -S mingw-w64-i686-libtool --noconfirm
+pacman -S mingw-w64-i686-make --noconfirm
+pacman -S mingw-w64-i686-zlib --noconfirm
+pacman -S mingw-w64-i686-libxml2 --noconfirm
+pacman -S mingw-w64-i686-dlfcn --noconfirm
+pacman -S mingw-w64-i686-autotools --noconfirm
+
+echo ""
+echo "*** All done ***"
+echo ""
